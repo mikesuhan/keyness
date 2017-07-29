7b01a75edb88e70772da17e7b77740ff82d766ff
Mike Suhan <mikesuhan@gmail.com>
2017-07-29 15:56:01 -0400

    fixed imports in __init__.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='keyness',
-    version='0.2',
+    version='0.3',
     packages=['keyness'],
     url='https://github.com/mikesuhan/keyness',
     license='MIT License',
