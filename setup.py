d02616b632ed747a70d9158647b38997f921d76b
Mike Suhan <mikesuhan@gmail.com>
2019-04-02 17:48:55 +0300

    Made it so type_dist and freq_dist were not both the same

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='Keyness',
-    version='0.24',
+    version='0.25',
     packages=['keyness'],
     url='https://github.com/mikesuhan/keyness',
     license='MIT License',
